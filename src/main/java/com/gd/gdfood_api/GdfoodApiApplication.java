08c8911ef4381116a1dcf9cefbe5e8edf08c4134
Otávio Ferreira <ottvferreira@gmail.com>
2024-09-12 13:04:14 -0300

    Creating Project

---
+++ src/main/java/com/gd/gdfood_api/GdfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.gd.gdfood_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gd/gdfood_api/GdfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.gdfood_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
